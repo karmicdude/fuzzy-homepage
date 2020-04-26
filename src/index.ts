02a594dc5de6b225d2276e33b5584056f7c135ae
Alexander Danilenko <alexander.danilenko@netprotect.com>
2020-04-26 14:47:16 +0900

    service worker path fix

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import './components/app/hp-app';
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('/service-worker.js')
+    .register('./service-worker.js')
       .then(registration => {
         console.log('SW registered: ', registration);
       })
